5cc466176a64c4a8ca324686dfe97e806c41bc21
xqzr <34030394+xqzr@users.noreply.github.com>
2023-08-27 03:52:51 +0800

    Update cs9rpmdownload.sh

--- scripts/cs9rpmdownload.sh
+++ scripts/cs9rpmdownload.sh
@@ -2,7 +2,7 @@
 
 echo "Preparing to download RPMs"
 echo "Setting up wget..."
-sudo dnf install wget -y
+sudo dnf install curl wget -y
 
 echo "Creating directory ~/cs8to9/el9"
 mkdir -p ~/cs8to9/el9
@@ -11,7 +11,7 @@ mkdir -p ~/cs8to9/epel9
 
 echo "Downloading RPMs..."
 cd ~/cs8to9/epel9
-wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+curl -LO https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
 wget https://dl.fedoraproject.org/pub/epel/epel-next-release-latest-9.noarch.rpm
 
 cd ~/cs8to9/el9
